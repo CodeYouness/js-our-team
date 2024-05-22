4c8484694a67f1b9e8a6bc764a3834b09719a560
CodeYouness <ylijassi@gmail.com>
2024-05-22 19:04:08 +0200

    created element for every part of the card (img, h5 and p)

--- js/script.js
+++ js/script.js
@@ -6,6 +6,17 @@ Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 */
+
+/*<div class="col-4">
+        <div class="card">
+            <img src="./img/wayne-barnett-founder-ceo.jpg" class="card-img-top" alt="ceo">
+        <div class="card-body text-center">
+            <h5 class="card-title">Wayne Barnett</h5>
+            <p class="card-text">Founder & CEO</p>
+        </div>
+    </div>
+</div>*/
+
 const ourTeam = [{
     teamName: 'Wayne Barnett',
     position: 'Founder & CEO',
@@ -37,8 +48,37 @@ const ourTeam = [{
     img: 'barbara-ramos-graphic-designer.jpg'
 }]
 
+const containerEL = document.querySelector('div#main-container')
+
 
 
 for (let key in ourTeam) {
+    //contenitore di tutto
+    const teamEl = document.createElement('div')
+    teamEl.classList.add('col-4')
+
+    //contenitore con classe card
+    const cardEl = document.createElement('div')
+    cardEl.classList.add('card')
+
+    //contenitore immagine
+    const imgEl = document.createElement('img')
+    imgEl.classList.add('card-img-top')
+    imgEl.src = ourTeam[key].img
+
+    //contenitore body della card
+    const bodyCardEl = document.createElement('div')
+    bodyCardEl.classList.add('card-body', 'text-center')
+
+    //h5 della card con nome schiavo
+    const teamNameEl = document.createElement('h5')
+    teamNameEl.classList.add('card-title')
+    teamNameEl.textContent = ourTeam[key].teamName
+
+    //<p> della card con posto di lavoro dello schiavo
+    const positionEl = document.createElement('p')
+    positionEl.classList.add('card-text')
+
+
     console.log(ourTeam[key])
 }
